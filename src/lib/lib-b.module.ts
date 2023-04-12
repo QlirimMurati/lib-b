3740e519395b9f2b8c57e1cc2361d95a0cfcc9a9
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-04-12 14:29:52 -0600

    feat(lib-b): secondary entry points

--- src/lib/lib-b.module.ts
+++ src/lib/lib-b.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LibAModule } from '@test/lib-a';
+import { ComponentAModule } from '@test/lib-a/components/src/lib/component-a';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LibAModule, ComponentAModule],
 })
 export class LibBModule {}
